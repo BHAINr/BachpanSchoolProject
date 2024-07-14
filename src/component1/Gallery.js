90c31121e8383c6ac00f33e6bbbe6b4ecbc3d633
navedkhan63705 <123861289+navedkhan63705@users.noreply.github.com>
2024-07-14 13:33:36 +0000

    git add

---
+++ src/component1/Gallery.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Gallery.css';
+
+const Gallery = () => {
+    return (
+        <div className='sidebar'>
+            <h2>Years of festival</h2>
+            <ul>
+                <li>2023-2024</li>
+                <li>2022-2023</li>
+                <li>2021-2022</li>
+                <li>2020-2021</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Gallery;

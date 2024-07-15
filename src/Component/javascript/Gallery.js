6f2162bebc1c6a164869b0e8db0da12612d0ec4f
navedkhan63705 <123861289+navedkhan63705@users.noreply.github.com>
2024-07-15 17:59:17 +0000

    push recent update file

--- src/Component/javascript/Gallery.js
+++ src/Component/javascript/Gallery.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import './Gallery.css';
+import '/workspaces/BachpanSchoolProject/src/Component/style/Gallery.css';
+import { render } from '@testing-library/react';
 
 const Gallery = () => {
+     const render=()=>{
+        alert("run");
+     }
     return (
         <div className='sidebar'>
             <h2>Years of festival</h2>
             <ul>
-                <li>2023-2024</li>
+                <li onClick={render}>2023-2024</li>
                 <li>2022-2023</li>
                 <li>2021-2022</li>
                 <li>2020-2021</li>
--- src/Component/javascript/Navbar.js
+++ src/Component/javascript/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './Navbar.css';
+import '/workspaces/BachpanSchoolProject/src/Component/style/Navbar.css';
 // import logo from './images/bachpanlogo.png'; 
 
 function Navbar() {
